774bb1c904e4da5a390e51cdf42a7275d7498df1
Andrew Tu <drewtu2@yahoo.com>
2020-02-24 17:44:52 -0500

    managed to add another convolutional layer

--- hw2_cnn/CNNClassifier.py
+++ hw2_cnn/CNNClassifier.py
@@ -12,6 +12,8 @@ class CNNClassifier(nn.Module):
             n_input *= shape
 
         self.conv1 = nn.Conv2d(3, 32, 5)            # conv. layer 1
+        #self.conv2 = nn.Conv2d(32, 16, 5, padding=(5, 5))           # conv. layer 2
+        #self.conv2 = nn.Conv2d(32, 16, 5, padding=(5//2, 5//2))           # conv. layer 2
         self.conv2 = nn.Conv2d(32, 16, 5)           # conv. layer 2
         self.conv3 = nn.Conv2d(16, 16, 5, padding=(5//2, 5//2))           # conv. layer 3
         
@@ -23,9 +25,10 @@ class CNNClassifier(nn.Module):
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
+        print(x.shape)
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        #x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv3(x)))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
